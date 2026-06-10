727f82ff74d3c8518b0e4c268126aa4dae2686a2
erk-smv <smv@erk.coach>
2026-06-10 19:21:22 +0200

    erasing the double get_flavor function

--- facturx_validator/models/facturx_analysis.py
+++ facturx_validator/models/facturx_analysis.py
@@ -242,28 +242,6 @@ class FacturxAnalysis(models.Model):
         ORDERX_TYPES, string='XMP Order-X Type', readonly=True, copy=False)
     afrelationship = fields.Char(string='AFRelationship', readonly=True, copy=False)
 
-
-def get_flavor(xml_etree):
-    """Extension locale de get_flavor() (upstream: akretion/factur-x).
-    Ajoute la détection des formats non couverts par la lib pip."""
-    logger.debug('get_flavor: tag=%s', xml_etree.tag)
-    tag = xml_etree.tag
-    if '{' in tag:
-        ns = tag[1:tag.index('}')]
-        if ns in UBL_ROOT_NAMESPACES:
-            logger.debug('get_flavor: result=ubl')
-            return 'ubl'
-        if ns == CDAR_XML_NAMESPACES['rsm']:
-            logger.debug('get_flavor: result=cdar')
-            return 'cdar'
-        # TODO e-Reporting: ajouter ici le root namespace quand le cahier des charges sera disponible
-        # if ns == 'urn:...:e-Reporting:...':
-        #     return 'ereporting'
-    flavor = _get_flavor_orig(xml_etree)
-    logger.debug('get_flavor: result=%s', flavor)
-    return flavor
-
-
     @api.model
     def create(self, vals):
         if vals.get('name', '/') == '/':
