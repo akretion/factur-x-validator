59fa5f2eed0bb8f1aadd748aca7fab76753c3c21
erk-smv <smv@erk.coach>
2026-06-11 11:39:11 +0200

    add France_RFE as submodule for schemas

--- facturx_validator/schemas/UBL/XSD/UBL-2.1/val/test.sh
+++
@@ -1,4 +0,0 @@
-sh validate.sh ../xsd/maindoc/UBL-Order-2.1.xsd order-test-good.xml
-sh validate.sh ../xsd/maindoc/UBL-Order-2.1.xsd order-test-bad1.xml
-sh validate.sh ../xsd/maindoc/UBL-Order-2.1.xsd order-test-bad2.xml
-
--- facturx_validator/schemas/UBL/XSD/UBL-2.1/val/w3cschema.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -jar xjparse.jar -c catalog.xml -S %1 %2
