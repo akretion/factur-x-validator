980b0e973f48e3c7d41f6193a52e9213667d8c02
Sebastien Maurines <sebastien.maurines@akretion.com>
2026-06-29 17:32:25 +0200

    1-correcting the dictionary in order to detect the CTC-FR for CII --- 2-correcting the report naming in order to incluse the date of day

--- facturx_validator/models/facturx_analysis.py
+++ facturx_validator/models/facturx_analysis.py
@@ -83,7 +83,7 @@ _PROFILES_DEF = [
     ('orderx_extended',         'Order-X Extended'),
     ('cii_en16931',             'CII EN 16931'),
     ('cii_extended',            'CII Extended'),
-    ('cii_extended_ctc_fr',     'CII Extended-CTC-FR'),
+    ('cii_extended_ctc_fr',     'CII Extended-CTC-FR',      'urn:cen.eu:en16931:2017#conformant#urn.cpro.gouv.fr:1p0:extended-ctc-fr'),
     ('ubl_en16931',             'UBL EN 16931',             'urn:cen.eu:en16931:2017'),
     ('ubl_extended_ctc_fr',     'UBL Extended-CTC-FR',      'urn:cen.eu:en16931:2017#conformant#urn.cpro.gouv.fr:1p0:extended-ctc-fr'),
     ('cdar_ctc_fr',             'CDAR CTC-FR'),
